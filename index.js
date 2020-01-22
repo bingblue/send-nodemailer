8363f389b7c28cb88c863a4b8fa55d21e4486882
gavin.gao <gaojb@mti-sh.cn>
2020-01-22 11:54:34 +0800

    add env

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const data = {
   from: process.env['INPUT_FROM'],
   to: process.env['INPUT_TO'],
   subject: process.env['INPUT_SUBJECT'],
-  text: JSON.parse(process.env['INPUT_TEXT']),
+  text: process.env['INPUT_TEXT'],
   html: process.env['INPUT_HTML']
 }
 if (process.env['INPUT_PATH']) {
